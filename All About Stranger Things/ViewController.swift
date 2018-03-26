a4d4284e375c5170e6cd40644d322f45a9913729
Jackie Blizzard <jackieblizzard@jackies-macbook-air.local>
2018-03-26 16:50:20 -0400

    echo "# GWC-iPhone-Project-1-My-Info-App" >> README.md git init git add README.md git commit -m "first commit" git remote add origin https://github.com/JacqualynBlizzard/GWC-iPhone-Project-1-My-Info-App.git git push -u origin master

--- All About Stranger Things/ViewController.swift
+++ All About Stranger Things/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var quotes: [String] = ["Mornings are for coffee and contemplation", "Lando!", "Friends  Don't Lie", "I'm stealthy, like a ninja"]
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBAction func getQuote(_ sender: Any) {
+        var quoteToDisplay: Int = Int(arc4random_uniform(4))
+        quoteLabel.text = quotes[quoteToDisplay]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
