1314d70e3f91b5d0b8bd986fe76a484874ab87e2
Jacqualyn Blizzard <jacqualyn.blizzard@gmail.com>
2019-01-14 15:56:24 -0700

    Add files via upload
    Updated to Swift 4.2 .random() method

--- All About Stranger Things/ViewController.swift
+++ All About Stranger Things/ViewController.swift
@@ -9,23 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+
+    @IBOutlet weak var quoteLabel: UILabel!
+    
     var quotes: [String] = ["Mornings are for coffee and contemplation", "Lando!", "Friends  Don't Lie", "I'm stealthy, like a ninja"]
     
-    @IBOutlet weak var quoteLabel: UILabel!
-    @IBAction func getQuote(_ sender: Any) {
-        var quoteToDisplay: Int = Int(arc4random_uniform(4))
-        quoteLabel.text = quotes[quoteToDisplay]
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func getQuote(_ sender: Any) {
+        var quoteToDisplay = Int.random(in: 0..<4)
+        quoteLabel.text = quotes[quoteToDisplay]
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
